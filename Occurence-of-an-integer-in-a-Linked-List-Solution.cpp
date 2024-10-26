2d93e55494c8c9f7257a7f7b4178ee690de502ae
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2024-10-26 20:14:14 +0530

    Create Occurence-of-an-integer-in-a-Linked-List-Solution.cpp

---
+++ Occurence-of-an-integer-in-a-Linked-List-Solution.cpp
@@ -0,0 +1,97 @@
+############################################################################  Question  ####################################################################################################
+
+Given a singly linked list and a key, count the number of occurrences of the given key in the linked list.
+
+Examples:
+
+Input: Linked List: 1->2->1->2->1->3->1, key = 1
+
+Output: 4
+Explanation: 1 appears 4 times. 
+Input: Linked List: 1->2->1->2->1, key = 3
+
+Output: 0
+Explanation: 3 appears 0 times.
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ number of nodes, key ≤ 105
+1 ≤ data of node ≤ 105
+
+
+############################################################################  Solution  ####################################################################################################
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+  Node is defined as
+struct node
+{
+    int data;
+    struct node* next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}*head;
+*/
+class Solution {
+  public:
+    int count(struct Node* head, int key) {
+          int cnt=0;
+        while(head){
+            if(head->data==key) cnt++;
+            head=head->next;
+        }
+        return cnt;
+    }
+
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        int key;
+        cin >> key;
+        cin.ignore();
+        Solution ob;
+        cout << ob.count(head, key) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
